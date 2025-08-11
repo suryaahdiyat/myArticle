ad098c076d1a7d762379c1d7e724919681cc3f9e
suryaahdiyat <hackandersonwite@gmail.com>
2025-08-11 09:27:59 +0800

    forcing to be https at production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
         Gate::define('admin', function (User $user) {
             return $user->role === 'su';
         });
